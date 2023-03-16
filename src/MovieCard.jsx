7a5b921a0b61210e1c048411ee24a86b01444ee9
MK884 <marchantkhalid7810@gmail.com>
2023-03-16 19:15:17 +0530

    update UI

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,22 +1,44 @@
-import React from 'react';
+import React from "react";
 
 const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
   return (
-    <div className="movie" key={imdbID}>
-      <div>
-        <p>{Year}</p>
-      </div>
+    <div className="card" key={imdbID}>
+      <div className="content">
+        <div className="back">
+          <div className="back-content">
+            <img
+              src={
+                Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"
+              }
+              alt={Title}
+            />
+          </div>
+        </div>
+        <div className="front">
+          <div className="img">
+            <img
+              src={
+                Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"
+              }
+              alt={Title}
+            />
+          </div>
 
-      <div>
-        <img src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} alt={Title} />
-      </div>
-
-      <div>
-        <span>{Type}</span>
-        <h3>{Title}</h3>
+          <div className="front-content">
+            <small className="badge">{Year}</small>
+            <div className="description">
+              <div className="title">
+                <p className="title">
+                  <strong>{Title}</strong>
+                </p>
+              </div>
+              <p className="card-footer">{Type}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default MovieCard;
